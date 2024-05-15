d9001d5cd640cd7642564f35605770d57173b148
Gabriel Zanella <zanella.gabriel@hotmail.com>
2024-05-15 11:48:27 -0300

    Changes to be committed: 	modified:   app/__init__.py 	modified:   app/__pycache__/__init__.cpython-311.pyc#	modified:   app/__pycache__/routes.cpython-311.pyc 	modified:   app/models/__pycache__/usuarios.cpython-311.pyc 	modified:   app/models/usuarios.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,17 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'sua_chave_secreta'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Athon123@localhost/ticket_db'
 
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+db.init_app(app)
+bcrypt.init_app(app)
+login_manager.init_app(app)
 
-from app.models import usuarios
+from app.models.usuarios import Usuario
 from app.routes import *
+
+def criar_usuario_fixo():
+    from app.models.usuarios import Usuario
+
+    if not Usuario.query.filter_by(nome_usuario='admin@admin.com').first():
+        hashed_password = bcrypt.generate_password_hash('admin123').decode('utf-8')
+        novo_usuario = Usuario(nome_usuario='admin@admin.com', senha=hashed_password)
+        db.session.add(novo_usuario)
+        db.session.commit()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Usuario.query.get(int(user_id))
+
+
 
 
--- app/models/usuarios.py
+++ app/models/usuarios.py
@@ -1,16 +1,9 @@
-from app import db, login_manager
+from app import db
 from flask_login import UserMixin
-
-@login_manager.user_loader
-def load_user(user_id):
-    return Usuario.query.get(int(user_id))
 
 class Usuario(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    nome_usuario = db.Column(db.String(150), unique=True, nullable=False)
-    senha = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"Usuario('{self.nome_usuario}')"
+    nome_usuario = db.Column(db.String(50), unique=True, nullable=False)
+    senha = db.Column(db.String(100), nullable=False)
 
 
